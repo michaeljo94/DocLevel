81fcb60e3544d9fbfd26568bfad097fe0b4b074b
Michael Johann <michaeljohann94@googlemail.com>
2020-06-03 21:26:32 +0200

    DL-1 Grundgerüst für das Project erstellen
    Foundation for DocMgr added

---
+++ DocMgr/src/main/java/de/mjohann/docmgr/DocMgr.java
@@ -0,0 +1,22 @@
+package de.mjohann.docmgr;
+
+/**
+ * The type Doc mgr.
+ */
+public class DocMgr {
+    /**
+     * Constructor for DocMgr
+     */
+    public DocMgr() {
+        //TODO Build DocMgr
+    }
+
+    /**
+     * DocMgr EntryPoint
+     * @param args
+     *         the input arguments
+     */
+    public static void main(String[] args) {
+        DocMgr docMgr = new DocMgr();
+    }
+}
